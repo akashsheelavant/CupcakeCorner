7da1659fef601342b5b34db0346410e9f517f554
Akash Sheelavant <asheelavant@walmart.com>
2024-07-17 16:52:24 +0530

    Initial commit.

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Akash Sheelavant on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
