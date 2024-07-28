ab414c6a028c2088df816e38f6435369aeba03cb
Akash Sheelavant <asheelavant@walmart.com>
2024-07-28 21:13:55 +0530

    Taking basic order details

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Akash Sheelavant on 7/28/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Akash Sheelavant on 7/28/24.
+//
+
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
